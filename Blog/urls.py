202ba78e37c1714c76e95c1c41a21e1aef549907
Satyam Jha <10satyam.jha@gmail.com>
2021-06-03 12:02:35 +0530

    some new functions

--- Blog/urls.py
+++ Blog/urls.py
@@ -7,5 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('blog', home ,name='homepage'),
     path('blog/<str:pk>',article , name = 'article'),
-    path('edit/<str:pk>' , edit, name= 'editpage')
+    path('edit/<str:pk>' , edit, name= 'editpage'),
+    path('<int:pk>' , writers , name= 'writer'),
+    path('new',new_post, name= 'new_post'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Blog/views.py
+++ Blog/views.py
@@ -12,11 +12,12 @@ def home(request):
     return render(request,'home.html',context=context)
 
 
-def author_details(request,pk):
-    authors = author.objects.get(name = pk)
-    context = {'authors':authors}
-
-    return render(request, 'author.html' , context=context)
+def writers(request,pk):
+    author_detail = author.objects.get(pk = pk)
+    blogs = post.objects.filter(contributor = author_detail.id)
+    context = {'writer': author_detail,
+                'blogs':blogs}
+    return render(request, 'writer.html' ,context= context)
 
 def article(request,pk):
     blog = post.objects.get(title = pk.replace('-' , ' '))
@@ -37,3 +38,15 @@ def edit(request,pk):
                 'form' : form} 
 
     return render(request , 'write.html' , context = context)
+
+
+def new_post(request):
+    form = Postform()
+    if request.method == 'POST' :
+        form = Postform(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/')
+    context = {'form':form}
+
+    return render(request , 'write.html' , context= context)
